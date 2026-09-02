587f6a4f131cef32b7757720bbd09d5945f6e9e1
Niket Gupta <niketgupta1@gmail.com>
2026-09-02 14:03:48 -0500

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 21
+float_type = 98.5
+bool_type = False
+string_type = "NIK"
+list_type = [1,2,3]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
